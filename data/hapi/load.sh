47e74561fc0650bd9f7d04f25f55cbefb6b904bc
Carl Anderson <carl.anderson@gmail.com>
2024-06-01 10:25:49 -0500

    feat: rename files with underscores (stupid synthea)

--- data/hapi/load.sh
+++ data/hapi/load.sh
@@ -8,8 +8,8 @@ set -o pipefail
 set -u
 
 # Number of concurrent file loaders.
-CONCURRENCY=4
-FHIR_URL="http://localhost:8080/fhir"
+CONCURRENCY=6
+export FHIR_BASE="http://localhost:8080/fhir"
 
 # Allow the script to be invoked from other directories and still work.
 THIS_DIR="$( dirname ${0} )"
@@ -19,7 +19,7 @@ if (( $# == 0 )); then
   cat <<EOM
 Usage: $( basename ${0} ) dir-name[s]
 
-Loads FHIR bundles from a directory into: ${FHIR_URL}.
+Loads FHIR bundles from a directory into: ${FHIR_BASE}.
 
 Up to ${CONCURRENCY} parallel uploads happen at once.
 EOM
@@ -36,27 +36,59 @@ ${THIS_DIR}/start.sh >/dev/null
 
 
 ##
-# POST a file to the server.
+# Print a summary of the load results for a loaded file.
 #
-function load() {
-  echo "Loading: ${1}"
-  curl \
-    -X POST \
-    -H "X-Upsert-Extistence-Check: disabled" \
-    -H "Content-Type: application/json" \
-    ${FHIR_URL} \
-    --data-binary "@${1}" \
-    &> "${1}.log"
+function summarize() {
+  local filename="${1}"
+  grep '"status"' "${filename}".log \
+    | sort \
+    | uniq -c \
+    | sed -e 's|[ ]*"status": "| - status: |;s:..$::' \
+    | \
+  while read status; do
+    echo "FILE: ${filename}: ${status}"
+  done
 }
 
 
 ##
-# Load a bunch of files in parallel.
+# POST a file to the server.
+#
+function load() {
+
+  # Remove single quotes from the filename, if present.
+  local file_folder="$( dirname "${1}" )"
+  local filename="$( basename "${1}" )"
+  filename="${file_folder}/${filename//'/_}"
+  if [[ ${1} != ${filename} ]]; then
+    echo "Renaming: ${1} to remove single quotes in filename..."
+    mv -v "${1}" "${file_folder}/${filename}"
+  fi
+
+  # Load the file.
+  curl \
+    --no-progress-meter \
+    --fail-with-body \
+    -X POST \
+    -H "X-Upsert-Extistence-Check: disabled" \
+    -H "Content-Type: application/json" \
+    ${FHIR_BASE} \
+    --data-binary "@${filename}" \
+    &> "${filename}.log"
+
+  # Print a summary of the load results for the loaded file.
+  summarize "${filename}"
+}
+
+
+##
+# Load files in parallel, maintaining no more than CONCURRENCY simultaneous uploads.
 #
 function load_files_matching() {
   local dir="${1}"
   local pattern="${2}"
   export -f load
+  export -f summarize
   find ${dir} -maxdepth 1 -mindepth 1 -type f -name "${pattern}" -print0 \
     | xargs -0 -n1 -P${CONCURRENCY} bash -c 'load "${@}"' "_"
 }
@@ -66,7 +98,8 @@ for dir in ${@}; do
   # Synthea usually places organization and practionioner files into separate
   # bundle files that start with lowercase letters.  We want to load these
   # first.
-  load_files_matching ${dir} '[a-z]*.json'
+  load_files_matching ${dir} 'organization*.json'
+  load_files_matching ${dir} 'practitioner*.json'
   # Load patient files next.
   load_files_matching ${dir} '[^a-z]*.json'
 done
