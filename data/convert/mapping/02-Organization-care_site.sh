ee9de271e4e6a749b559769122c268427dd80155
Carl Anderson <carl.anderson@gmail.com>
2024-06-03 09:07:54 -0500

    fix: read only the first address from Organization

--- data/convert/mapping/02-Organization-care_site.sh
+++ data/convert/mapping/02-Organization-care_site.sh
@@ -3,22 +3,22 @@
 # Converts FHIR XXX resources to OMOPCDM YYY records.
 #
 source _common.sh
-echo "starting"
+
 simple_map '
 # FHIR                # OMOP
-#---------------------#-------------------------------#
-.id,                  # location_id
-.address.line[0],     # address_1
-.address.line[1],     # address_2
-.address.city,        # city
-.address.state,       # state
-.address.postalCode,  # zip
-null                  # county - not available in FHIR
-.name,                # location_source_value
-4330442,              # country_concept_id
-"USA",                # country_source_value
-.position.latitude,   # latitude
-.position.longitude   # longitude
+#------------------------#-------------------------------#
+.id,                     # location_id
+.address[0].line[0],     # address_1
+.address[0].line[1],     # address_2
+.address[0].city,        # city
+.address[0].state,       # state
+.address[0].postalCode,  # zip
+null                     # county - not available in FHIR
+.name,                   # location_source_value
+4330442,                 # country_concept_id
+"USA",                   # country_source_value
+.position.latitude,      # latitude
+.position.longitude      # longitude
 '
 
 exit 0
