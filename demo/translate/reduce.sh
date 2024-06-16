c00fd1c3365abaec7cb34df9b7cf698fa3bb1f98
Carl Anderson <carl.anderson@gmail.com>
2024-06-15 21:55:39 -0500

    feat: completed reducer and loader

--- demo/translate/reduce.sh
+++ demo/translate/reduce.sh
@@ -19,7 +19,8 @@ function omop_fhir_mappings() {
   # of OMOPCDM and list the destination table name followed by all the
   # possible FHIR Resource types that generate input for that table.
   #
-  # For example:
+  # For example, this function emits rows like:
+  #   location Location
   #   provider Practitioner PractitionerRole
   #
   grep -A1 '^\[' map/*.jq \
@@ -69,12 +70,29 @@ function get_resource_filenames() {
 function reduce() {
   local table_name=${1}
   local resource_files=$( get_resource_filenames ${2} )
-  [ -z "${resource_files}" ] && return
+
+  # Skip tables that have produced no source files.
+  if [[ -z ${resource_files} ]]; then
+    echo "    SKIPPING ${table_name}: no staged source files!"
+    return 0
+  fi
+
+  # Avoid the thundering herd, if possible, by sleeping a tiny bit.
+  sleep 0.${RANDOM}
+
+  # Use awk to reduce rows with identical first columns, which should be the
+  # primary key column for the destination table.
+  echo "    REDUCING ${table_name}.tsv..."
   sed -n "s/^${table_name}\t//p" ${resource_files} \
     | sort -n \
-    | awk '
-BEGIN { FS = "\t" }
+    | awk -F "\t" '
+#
+# inline.awk: reduces row fragments into loadable rows.
+#
 
+##
+# Emit the merged row and move on to the next.
+#
 func emit() {
   # Print the merged row.
   ORS=""; print row[1]; for (i = 2; i <= NF; ++i) print FS row[i];
@@ -83,12 +101,15 @@ func emit() {
   for (i = 1; i <= NF; ++i) row[i] = $i
 }
 
+##
+# Merge this row into the previous one.
+#
 func merge() {
   for (i = 1; i <= NF; ++i) {
     if (row[i] == "") {
       row[i] = $i
     } else if ($i != "" && row[i] != $i) {
-      system("echo MERGE CONFLICT: " $i " != " row[i] " 1>&2")
+      system("echo ${table_name}: MERGE CONFLICT: " $i " != " row[i] " 1>&2")
     }
   }
 }
@@ -97,25 +118,56 @@ func merge() {
   # Initialize row for the first line of input.
   if (row[1] == "") { row[1] = $1 }
 
-  # Reduce this line into the previous one.
+  # Merge this line into the previous one, or emit the previous row.
   if ($1 == row[1]) { merge() } else { emit() }
 }
 
+# Be sure to emit the final row.
 END { emit() }
 ' \
   > data-omop/${table_name}.tsv
-
-  # Load the data into the database.
-  sqlite3 cdm.db <<SQL
-.mode ascii
-.separator "\t" "\n"
-.import data-omop/${table_name}.tsv ${table_name}
-SQL
 }
 
 
-# Get the mapped table_names to source files for reducing.
+# Start a timer.
+start=$(( $( date +%s ) ))
+
+#
+# Use xargs to build tsv files in parallel.
+#
+export -f get_resource_filenames
+export -f reduce
+
+# Reduce the stg-FHIR.tsv files to OMOP.tsv files for loading.
+echo "Reducing staged OMOP framents into whole rows for loading..."
+omop_fhir_mappings | \
+while read omop resources; do
+  # Insert \0 characters between tokens for xargs to handle the variable
+  # number of source files per table destination.
+  echo -en "${omop}\0${resources}\0"
+done \
+  | xargs -0 -n2 -P${CONCURRENCY} bash -c 'reduce "${@}"' _
+
+# Load the data into the database, one by one.
+echo "Loading reduced OMOP into CDM database..."
 omop_fhir_mappings | while read omop resources; do
-  echo "Coalescing ${omop} data..."
-  reduce "${omop}" "${resources}"
+  tsv="data-omop/${omop}.tsv"
+  [ -e "${tsv}" ] || continue
+  echo "    IMPORTING ${omop}..."
+  sqlite3 cdm.db <<SQL
+.mode ascii
+.separator "\t" "\n"
+.import ${tsv} ${omop}
+SQL
 done
+
+# Clean up.
+echo "Deleting staged OMOP fragments..."
+rm -v data-omop/stg-*.tsv | sed -e 's:^:    DELETING :'
+
+# DONE!!!
+cat <<DONE
+Merged and loaded rows:
+$( wc -l data-omop/*.tsv )
+Completed in $(( $( date +%s ) - start )) seconds!
+DONE
